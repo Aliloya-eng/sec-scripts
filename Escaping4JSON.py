80d99d961bebc9dfccb4c60f8f5363cb1cd2e02a
unknown <alihassan.courses@gmail.com>
2022-05-03 12:29:30 +0200

    adding more subdomains to subdomains.txt +++ adding coverage options to PreScanner.py +++ adding NEW script: Escaping4JSON.py

---
+++ Escaping4JSON.py
@@ -0,0 +1,18 @@
+import json
+import sys
+
+print('''
+    Usage: Escaping4JSON.py input-file.txt output-file.txt
+''')
+
+file = []
+input = sys.argv[1]
+output = sys.argv[2]
+with open(input,"r") as readf:
+    for l in readf.readlines():
+        file.append((json.dumps(l)[1:-3])+"\n")
+readf.close()
+print(file)
+with open(output,"w") as writef:
+    writef.writelines(file)
+writef.close()
--- PreScaner.py
+++ PreScaner.py
@@ -2,7 +2,22 @@ import sys
 import os
 
 vars = sys.argv[1:]
-if "-url" not in vars or "-domain" not in vars or "-h" in vars:
+if "-h" in vars:
+    print('''
+    PreScanner is a script written in Python3 to automate some of the basic recon processes. It simply calls other tools and prints the outout into text files in a subdirectory that holds the name of the examined domain.
+    Usage example:   python PreScaner.py -url http://sub1.sub2.target.test/ -domain sub2.target.test --A     (python3)
+    
+    Target
+        -url        the URL of the target
+        -domain     the domain of the target
+    Coverage
+        --A         exclude active(url) scans
+        --P         exclude passive(domain) scans
+        --O         overwrite previous scans (for domain scans only, url scans are overwritten anyway if not excluded)
+    ''')
+    exit()
+
+if  "-url" not in vars or "-domain" not in vars:
     print('''
     Please enter the url and the domain of the target
     Usage example:   python PreScaner.py -url http://sub1.sub2.target.test/ -domain sub2.target.test     (python3)
@@ -12,30 +27,36 @@ if "-url" not in vars or "-domain" not in vars or "-h" in vars:
 url = vars[vars.index("-url")+1]
 url = url.replace("https","http")
 domain = vars[vars.index("-domain")+1]
-os.system("mkdir {}".format(domain))
+passive = "--P" not in vars
+active = "--A" not in vars
+overwrite = "--O" in vars
 
-# DOMAIN
-## whois
-print("whois-ing")
-os.system("whois {} > {}/whois.txt".format(domain,domain))
-## dig
-print("dig-ing")
-os.system("dig {} > {}/dig.txt".format(domain,domain))
-## dnsrecon
-print("dnsrecon-ing")
-os.system("dnsrecon -d {} -a -s -b -y -k -w -z -t std > {}/dnsrecon.std.txt".format(domain,domain))
-os.system("dnsrecon -d {} -a -s -b -y -k -w -z -t crt > {}/dnsrecon.crt.txt".format(domain,domain))
-## sublister
-print("sublister-ing")
-os.system("sublist3r -d {} -v > {}/sublist3r.txt".format(domain,domain))
-## theHarvester
-print("theHarvester-ing")
-os.system("theHarvester -d {} -g -s -r -b all > {}/theHarvester.txt".format(domain,domain))
+if passive:
+    if overwrite or domain not in os.system("ls").split():
+        # DOMAIN - PASSIVE
+        os.system("mkdir {}".format(domain))
+        ## whois
+        print("whois-ing")
+        os.system("whois {} > {}/whois.txt".format(domain,domain))
+        ## dig
+        print("dig-ing")
+        os.system("dig {} > {}/dig.txt".format(domain,domain))
+        ## dnsrecon
+        print("dnsrecon-ing")
+        os.system("dnsrecon -d {} -a -s -b -y -k -w -z -t std > {}/dnsrecon.std.txt".format(domain,domain))
+        os.system("dnsrecon -d {} -a -s -b -y -k -w -z -t crt > {}/dnsrecon.crt.txt".format(domain,domain))
+        ## sublister
+        print("sublister-ing")
+        os.system("sublist3r -d {} -v > {}/sublist3r.txt".format(domain,domain))
+        ## theHarvester
+        print("theHarvester-ing")
+        os.system("theHarvester -d {} -g -s -r -b all > {}/theHarvester.txt".format(domain,domain))
 
-# URL
-## whatweb
-print("whatweb-ing")
-os.system("whatweb -a 3 -v {} > {}/whatweb.txt".format(url,domain))
-## nikto
-print("Nikto-ing")
-os.system("nikto -host {} > {}/nikto.txt".format(url,domain))
+if active:
+    # URL - ACTIVE
+    ## whatweb
+    print("whatweb-ing")
+    os.system("whatweb -a 3 -v {} > {}/whatweb.txt".format(url,domain))
+    ## nikto
+    print("Nikto-ing")
+    os.system("nikto -host {} > {}/nikto.txt".format(url,domain))
